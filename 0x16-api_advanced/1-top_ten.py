1ada56a2a0432eb5a984a9204de08a962ce20d2b
Matilop15 <3959@holbertonschool.com>
2022-05-31 19:09:58 -0700

    add tas k1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Querie to the reddit API"""
+import requests
+
+
+def top_ten(subreddit):
+    """returns the number of subscribers"""
+    reddit = 'http://reddit.com/r/{}/hot.json'
+    head = {'User-agent': 'root:mati:v1'}
+    res = requests.get(reddit.format(subreddit), headers=head)
+    if res.status_code != 200:
+        print("None")
+    else:
+        filter = res.json().get('data').get('children')
+        for i in filter:
+            print(i.get('data').get('title'))
