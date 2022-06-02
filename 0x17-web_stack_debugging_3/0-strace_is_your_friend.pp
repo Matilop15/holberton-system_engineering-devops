53dc90594c9ade2e6dce326800509880bc015cbc
Matilop15 <3959@holbertonschool.com>
2022-06-01 18:51:44 -0700

    add all task

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,13 @@
+# After to execute strace -p PID, I discover that
+# The error is in /var/www/html/wp-settings.php route
+# Open the file searching:
+# lstat("/var/www/html/wp-includes/class-wp-locale.phpp", 0x7ffd5b1f45a0) = -1 ENOENT (No such file or directory)
+# Change the extension file to php
+# Restart apache2 server
+
+exec {'Correct extension':
+command => "/bin/sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+}
+exec { 'Restart Apache2 Server':
+command => 'service apache2 restart',
+}
