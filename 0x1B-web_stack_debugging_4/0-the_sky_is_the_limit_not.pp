35225474cbc0bef42802c444cdfe9720f84acbf6
Matilop15 <3959@holbertonschool.com>
2022-06-09 13:37:02 -0700

    debug debug code :D

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # Script to increment ULIMIT traffic an Nginx server
 
 exec { 'Debuggg':
-  command => '/bin/sed -i \'s/ULIMIT="-n 15"/ULIMIT="-n 4096"/\' /etc/default/nginx',
+  command => 'usr/bin/env sed -i s/15/4096/ /etc/default/nginx',
 }
 exec { 'Restart_Nginx':
   command => '/usr/bin/env service nginx restart',
