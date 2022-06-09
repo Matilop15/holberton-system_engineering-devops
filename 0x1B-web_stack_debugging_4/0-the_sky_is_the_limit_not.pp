442d3fd2a2c77e96cbe8cd811f7d3b9b123ff593
Matilop15 <3959@holbertonschool.com>
2022-06-09 13:20:08 -0700

    fix code

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,8 @@
-#Script to increment ULIMIT traffic an Nginx server
+# Script to increment ULIMIT traffic an Nginx server
 
 exec { 'Debuggg':
   command => '/bin/sed -i \'s/ULIMIT="-n 15"/ULIMIT="-n 4096"/\' /etc/default/nginx',
-  '/usr/sbin/service nginx restart',
+}
+exec { 'Restart_Nginx':
+  command => '/usr/sbin/service nginx restart',
 }
