fe5ef72d61e1a6d36e79f7ff001ed501d7bb1abf
Matias López <91102932+Matilop15@users.noreply.github.com>
2022-06-10 14:37:30 -0300

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,8 @@
 # Script to increment ULIMIT traffic an Nginx server
 
-exec { 'Debuggg':
+exec { 'fix--for-nginx':
   command => 'usr/bin/env sed -i s/15/4096/ /etc/default/nginx',
 }
-exec { 'Restart_Nginx':
+exec { 'restart_nginx':
   command => '/usr/bin/env service nginx restart',
 }
